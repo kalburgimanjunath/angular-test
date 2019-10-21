a35958f157d1e6b3742302347337dc9cde3c83fb
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2019-10-21 15:06:56 +0530

    Create email-validator.directive.ts

---
+++ src/app/email-validator.directive.ts
@@ -0,0 +1,21 @@
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
+
+@Directive({
+  selector: '[isEmail]',
+  providers: [{provide: NG_VALIDATORS, useExisting: EmailValidatorDirective, multi: true}]
+})
+export class EmailValidatorDirective {
+
+  constructor() {
+
+  }
+
+  public validate(control: AbstractControl): {[key: string]: any} {
+    // tslint:disable-next-line: max-line-length
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    let valid = emailRegEx.test(control.value);
+    return control.value < 1 || valid ? null : {'isEmail': true};
+  }
+
+}
